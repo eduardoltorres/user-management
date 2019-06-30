014ea92bbf96425ec3413c898792266ad0a2bd73
Eduardo L. Torres <eltorrescuesta@gmail.com>
2019-06-30 12:38:02 +0200

    update server (index.js) file

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+const cors = require('cors');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const userRoutes = require('./routes/users');
+require('dotenv').config();
 
+const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html');
-});
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.Promise = Promise;
 
-app.use('/api/users', userRoutes);
+const userRoutes = require('./routes/users');
+app.use('/users', userRoutes);
 
 app.listen(port, () => {
   console.log(`APP IS RUNNING ON PORT ${port}`);
